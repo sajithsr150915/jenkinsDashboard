1d8fa4f1b9cfece0dc5eadd68cb583400b986109
sajith <sajithsr6@gmail.com>
2020-04-17 21:47:26 +0530

    added jacoco implementation, report in target\jacoco-report

--- src/main/java/com/jenkins/controller/DemoApplication.java
+++ src/main/java/com/jenkins/controller/DemoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages={
-		"com.jenkins.*"})
+		"com.*"})
 public class DemoApplication {
 
 	public static void main(String[] args) {
---
+++ src/test/java/com/jenkins/JacocoSpringBootMavenApplicationIT.java
@@ -0,0 +1,30 @@
+package com.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+//import org.junit.Assert;
+
+import org.springframework.web.client.RestTemplate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class JacocoSpringBootMavenApplicationIT {
+
+	@Test
+	void contextLoads() {
+		
+
+//RestTemplate restTemplate = new RestTemplate();
+     
+    //final String baseUrl = "http://localhost:8083/manageJenkins/";
+    //URI uri = new URI(baseUrl);
+ 
+    //ResponseEntity<String> result = restTemplate.getForEntity(uri, String.class);
+     
+    //Verify request succeed
+   // Assert.assertEquals(200, 200);
+	}
+
+}
+
