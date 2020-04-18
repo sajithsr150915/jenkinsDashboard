e0cef751e4843111dfbcfba1b4b77aedf1b5c79a
sajithsr150915 <63293987+sajithsr150915@users.noreply.github.com>
2020-04-18 23:03:26 +0530

    changed to retrieve all builds

--- src/main/java/com/jenkins/constants/Constants.java
+++ src/main/java/com/jenkins/constants/Constants.java
@@ -2,12 +2,12 @@ package com.jenkins.constants;
 
 public class Constants {
 
-	public static final String JENKINS_DETAILS_API_URL = "/api/json?tree=jobs[name,url,builds[number,result,duration,url,timestamp,actions[buildsByBranchName[*[*[*]]]]]]";
+	public static final String JENKINS_DETAILS_API_URL = "/api/json?tree=jobs[name,url,allBuilds[number,result,duration,url,timestamp,actions[buildsByBranchName[*[*[*]]]]]]";
 	public static final String SUCESS="Success";
 	public static final String FAILURE="Failure";
 	public static final String UAT_DEPLOYMENT = "UAT-deployment";
 	public static final String PROD_DEPLOYMENT = "PROD-deployment";
-	public static final String JENKINS_TESTDETAILS_API_URL = "/api/json?tree=jobs[name,url,builds[number,result,duration,url,actions[*]]]";
+	public static final String JENKINS_TESTDETAILS_API_URL = "/api/json?tree=jobs[name,url,allBuilds[number,result,duration,url,actions[*]]]";
 	public static final String TEST_REPORT_URL = "testReport";
 	public static final String ACCEPTANCE = "Acceptance";
 	public static final String TYPE_ALL = "ALL";
